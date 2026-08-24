11305e2514e16156b7f1c43e6cbc4d1e8fa4e6a7
damoon1379 <damoonalimanesh@gmail.com>
2026-08-24 06:01:47 -0700

    added api/nextauth for authentication

---
+++ types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import { DefaultSession } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+
+declare module "next-auth"{
+    interface Session {
+        user: {
+        id:string,
+        role:string
+        } & DefaultSession["user"]
+    }
+
+    interface User {
+        role:string
+    }
+} 
+declare module "next-auth/jwt" {
+    interface JWT{
+        role: string,
+        id: string
+    }
+}
+declare module "next-auth/adapters" {
+  interface AdapterUser {
+    role: string;
+  }
+}
